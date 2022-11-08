87054ad52448ea4b0b7ae78f32c9d55c9c4df579
colombe.fagart@epfedu.fr <33768@DESKTOP-O7O9TDJ.education.lakanal.epf.fr>
2022-11-08 15:17:26 +0100

    voici le début de mon TP3 heritage

---
+++ TP4-héritage-Colombe_Fagart/src/tp4/héritage/colombe_fagart/Arme.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package tp4.héritage.colombe_fagart;
+
+/**
+ *
+ * @author 33768
+ */
+public class Arme {
+    public String Nom = "";
+    public int atq_dm=0;
+   
+    public Arme(String Nome, int atq_dme ) {
+      
+      Nom=Nome;
+      atq_dm=atq_dme;
+}}
---
+++ TP4-héritage-Colombe_Fagart/src/tp4/héritage/colombe_fagart/TP4HéritageColombe_Fagart.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package tp4.héritage.colombe_fagart;
+
+/**
+ *
+ * @author 33768
+ */
+public class TP4HéritageColombe_Fagart {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
